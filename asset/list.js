4a003a285bcbdf60ee43acf3a5ebb40976adbf4c
nouveaupo <77199339+nouveaupo@users.noreply.github.com>
2023-11-11 09:11:18 +0700

    Create list.js

---
+++ asset/list.js
@@ -0,0 +1,39 @@
+const products = [
+  {
+  name: "Carton of cherries",
+  price: 4,
+  quantity: 1,
+  productId: 1111,
+    valueitem: 4,
+    instock: 100,
+      image: "images/cherry.jpg",
+},
+ {
+  name: "Bag of orange",
+  price: 10,
+  quantity: 1,
+  productId: 2222,
+   valueitem: 10,
+   instock: 100,
+     image: "images/orange.jpg",
+},
+  {
+  name: "Carton of strawberry",
+  price: 5,
+  quantity: 1,
+  productId: 3333,
+    valueitem: 5,
+    instock: 100,
+      image: "images/strawberry.jpg",
+},
+
+  {
+    name: "Bag of orange",
+    price: 10,
+    quantity: 1,
+    productId: 2222,
+     valueitem: 10,
+     instock: 100,
+       image: "images/orange.jpg",
+  }
+  ]
