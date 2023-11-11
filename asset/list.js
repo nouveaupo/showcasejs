9db282c372c00702e819339c9c61870736facf67
nouveaupo <77199339+nouveaupo@users.noreply.github.com>
2023-11-11 17:42:22 +0700

    Update list.js

--- asset/list.js
+++ asset/list.js
@@ -19,7 +19,17 @@ const products = [
  image:"asset/meuble4.jpg" ,
     title: "Natural Meuble" ,
     info: "Nice place to do working and relaxing on the same time"  ,
-  }
+  },
+   {
+    image:"asset/meuble1.jpg" ,
+    title: "Futuristic Meuble" ,
+    info: "My Meuble with artistic theme build with heart and full of functionality"  ,
+},
+ {
+  image:"asset/meuble2.jpg" ,
+    title: "My Beautiful Meuble" ,
+    info: "The scene and the beautiful landscape will force the creativity to build the meuble to the limit"  ,
+},
 
 
   ]
